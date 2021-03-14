083d92cfaece7c5ce1fe57ca6c8b1521ff8bfa4e
JPereirax <jailsondev70@gmail.com>
2021-03-14 02:22:15 -0300

    adding steps feature in route system

--- route/route.go
+++ route/route.go
@@ -1,8 +1,11 @@
 package route
 
 import (
+	"GOIF/component"
+	"GOIF/types"
 	"log"
 	"net/http"
+	"strings"
 )
 
 type Rest struct {
@@ -13,6 +16,7 @@ type Rest struct {
 type Route struct {
 	path   string
 	method string
+	steps  []component.Component
 }
 
 func (rest *Rest) NewRoute(path string, method string) *Route {
@@ -20,6 +24,26 @@ func (rest *Rest) NewRoute(path string, method string) *Route {
 		path:   rest.BasePath + path,
 		method: method,
 	}
+}
+
+func (route *Route) MultiStep(steps ...string) *Route {
+	for _, step := range steps {
+		route.Step(step)
+	}
+	return route
+}
+
+func (route *Route) Step(step string) *Route {
+	compound := strings.Split(step, ":")
+
+	for _, components := range component.RegisteredComponents {
+		if components.Schema == compound[0] {
+			components.Uri = compound[1]
+			route.steps = append(route.steps, *components)
+		}
+	}
+
+	return route
 }
 
 func (route *Route) Build() {
@@ -31,5 +55,21 @@ func (route *Route) Build() {
 		}
 
 		log.Printf("Received request from host '%s' to route '%s'.", request.RemoteAddr, request.Host + route.path)
+
+		for _, step := range route.steps {
+			context := component.ContextRequest{
+				Component: step,
+				HttpTransport: types.HttpTransport{
+					Request:  request,
+					Response: response,
+				},
+			}
+
+			transportProperty := step.Function(context)
+			request, response = transportProperty.Request, transportProperty.Response
+			if response == nil {
+				break
+			}
+		}
 	})
 }
